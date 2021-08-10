d0c0c336e26856934aec1ba9ea578bdcd0dc8c5f
Dara-Ekanth <daraekanth3@gmail.com>
2021-08-10 10:19:07 +0530

    added url and project_urls.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup,find_packages
 import os
 current_directory = os.path.abspath(os.path.dirname(__file__))
-#print(current_directory)
+
 with open(os.path.join(current_directory,"readme.txt")) as file:
     long_description = file.read()
-#print(long_description)
 
 VERSION = 0.1
 DESCRIPTION = "A todo app to understand the building packages."
@@ -16,6 +15,8 @@ setup(
     author_email="daraekanth3@gmail.com",
     description=DESCRIPTION,
     long_description=long_description,
+    url = "https://github.com/Dara-Ekanth/todo_custom_package",
+    project_urls = {"Bug Tracker":"https://github.com/Dara-Ekanth/todo_custom_package/issues"},
     packages=find_packages(),
     install_requires=['pandas'],
     keywords=['python','data collection','data science','ML','todo','custom','custom package'],
@@ -23,6 +24,8 @@ setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
